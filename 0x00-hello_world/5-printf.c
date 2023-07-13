0e9ac3daac0775df469bb2bc32c1c4f2fb45defa
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-13 15:45:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: A C program that prints with printf function
+ * Description: A C program that prints with printf function.
  *
  * Return: Always 0 (success)
 */
