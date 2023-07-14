131562fc7dc36de6c33eb4ec67c3e9d048243826
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-14 13:48:11 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
  * main - Entry point
  *
- * Description: print all alphabet letters
- *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-	char ch = 'a';
+	char c;
 
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		ch++;
-	}
-	putchar('/n');
-
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
 	return (0);
-
 }
