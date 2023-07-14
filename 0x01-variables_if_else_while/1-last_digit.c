e9ba81222a9140dee5210d362d97001221e74118
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-14 12:57:17 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,34 @@
-#include<stdlib.h>
-#include<time.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints a text according number
+ * Return: Always (success)
  *
- * Description: print the value of n status: 
- * greater than, is zero and is not less than 6.
- *
- * Return: Always 0 (Success)
 */
 
 int main(void)
+
 {
-        int n, digit;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-	digit = n % 10;
+	int n, lastd;
 
-        if (digit > 5)
-                printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-        else if (digit == 0)
-                printf("Last digit of %i is %i and is 0\n", n, digit);
-        else if (digit < 6 && digit !=0)
-                printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+	if (lastd > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+	return (0);
 }
+
