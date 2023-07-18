b8bd89ef4aaac8d71c02da9d600aba3236b40cac
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-18 12:28:57 -0700

    0-putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
-#include "main.h"
+
 
 /**
  * _putchar - writes the character c to sdtout
  *@c: The character to print
- *
  * Return: on success 1.
  * on error, -1 is returned, and errno is set appropriatly.
  */
