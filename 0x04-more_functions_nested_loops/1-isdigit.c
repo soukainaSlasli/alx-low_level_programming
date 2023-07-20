09ad129895117d3317d726a0bb9e99b1f13f1cf3
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-20 08:43:18 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if a character is digit
+ * @x: the number to be checked
+ * Return: 1 for digit character or 0 for any else
+ */
+
+int _isdigit(int x)
+
+{
+
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
