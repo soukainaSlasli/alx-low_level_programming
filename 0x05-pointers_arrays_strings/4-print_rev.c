d46534f611daf575253b4e579c117f3ff9036009
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-24 09:33:43 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+	
+	while (s[c] != '\0')
+	{
+	C++;
+	}
+	for (c -= 1; c >= 0; C--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
