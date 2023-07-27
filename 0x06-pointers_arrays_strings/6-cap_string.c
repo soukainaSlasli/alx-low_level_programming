5207b4a85b68b430cd65ec2c7eac4c77295e6837
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-27 13:36:11 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,43 +2,35 @@
 
 /**
  * cap_string - Capitalizes all words of a string
- * str: the string to be capitalized 
+ * @str: the string to be capitalized
  * Return: A pointer to the changed string
  */
-char *cap_string(char *);
+
+char *cap_string(char *str)
 {
 	int index = 0;
-	while (str[index])
-{
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
-if (str[index - 1] == '' | |
-str[index - 1]
-str[index - 1] == '\t' | |
-str[index - 1]
 
-str[index - 1]
-1]
-str[index 1]
-str[index
-str[index - 1]
-str[index - 1]
-str[index 1]
-str[index - 1]
-str[index 1]
-str[index 1]
-index == 0)
-index++;
--
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
 }
-return (str);
-str[index]
-' ||
-'\t'
-'\n'
-'?'
-THE
-'")'
-'{'
-''}"
-32;
