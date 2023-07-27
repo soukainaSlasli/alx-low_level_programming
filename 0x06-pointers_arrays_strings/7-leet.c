3425a2fa816429b5c272dadb7fa0cdad348de3f6
soukainaSlasli <soukaina.slasli.pro@gmail.com>
2023-07-27 13:42:36 -0700

    aa

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,12 +7,12 @@
 char *leet(char *n)
 {
 	int i, j;
-	char s1[] = "aAeEoOtTIL";
+	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0';  i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0;  j < 10;  j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
 			{
